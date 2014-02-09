9c8dc5e27f62a812adf18103e7b28e948304cbce
Igor Santos <igorsantos07@gmail.com>
2014-02-09 10:38:34 -0200

    Now using struct initializer functions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,5 @@
 #include <pebble.h>
 
-#define MAX_WORKOUTS 7
-#define MAX_EXERCISES 20
-
 typedef struct {
 	char *name;
 	int sets, reps, weight;
@@ -22,33 +19,29 @@ static char itoc(int number) {
 	return number + 64;
 }
 
-static void setWorkouts(int i) {
+static Exercise newExercise(char *name, int sets, int reps, int weight) {
+	Exercise ex = { .name = name, .sets = sets, .reps = reps, .weight = weight };
+	return ex;
+}
+
+static Workout newWorkout(int number, Exercise *exercises) {
+	Workout w = { .letter = itoc(number), .exercises = exercises };
+	++total_workouts;
+	return w;
+}
+
+static void setWorkout(int i) {
 	Exercise exs[3] = {
-		{ .name = "Ex 1", .sets = 4, .reps = 10, .weight = 35 },
-		{ .name = "Ex 2", .sets = 4, .reps = 12, .weight = 8 },
-		{ .name = "Abdominal", .sets = 3, .reps = 20, .weight = 0 }
+		newExercise("Abdominal", 4, 20, 0),
+		newExercise("Supino", 4, 12, 15),
+		newExercise("Tríceps testa", 4, 12, 35)
 	};
-	Workout workout = {
-		.letter = itoc(total_workouts++),
-		.exercises = exs
-	};
+	Workout workout = newWorkout(i, exs);
 
 	persist_write_data(i, &workout, sizeof(workout));
-
-	total_workouts = ++i;
 }
 
-static Exercise *getExercises() {
-	static Exercise exs[] = {
-		{ .name = "Ex 1", .sets = 4, .reps = 10, .weight = 35 },
-		{ .name = "Ex 2", .sets = 4, .reps = 12, .weight = 8 },
-		{ .name = "Abdominal", .sets = 3, .reps = 20, .weight = 0 }
-	};
-
-	return exs;
-}
-
-static Workout getWorkouts(int key) {
+static Workout getWorkout(int key) {
 	Workout read_data;
 
 	if (persist_exists(key))
@@ -73,18 +66,13 @@ static void begin(void) {
 	text_layer_set_font(numbers_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
 	text_layer_set_text_alignment(numbers_layer, GTextAlignmentLeft);
 
-	setWorkouts(1);
-	Workout my_workout = getWorkouts(1);
-	Exercise my_ex = my_workout.exercises[1];
-	// Exercise *all = getExercises();
-	// Exercise my_ex = all[1];
+	setWorkout(1);
+	Workout my_workout = getWorkout(1);
+	Exercise my_ex = my_workout.exercises[2];
 
 	char numbers[15], title[100];
-	// snprintf(numbers, 15, "%dx\n%d\n%dkg", 4, 10, 35);
-	// text_layer_set_text(exercise_layer, "Blabla Whiskas Sache");
-	// text_layer_set_text(titles_layer, "Sets:\nReps:\nWeight:");
 	snprintf(numbers, sizeof(numbers), "%dx\n%d\n%dkg", my_ex.sets, my_ex.reps, my_ex.weight);
-	snprintf(title, sizeof(title), "%s", my_ex.name);
+	snprintf(title, sizeof(title), "[%c] %s", my_workout.letter, my_ex.name);
 	text_layer_set_text(exercise_layer, title);
 	text_layer_set_text(titles_layer, "Sets:\nReps:\nWeight:");
 	text_layer_set_text(numbers_layer, numbers);
@@ -105,64 +93,3 @@ static void end(void) {
 int main(void) {
 	begin(); app_event_loop(); end();
 }
-
-
-
-	// int e = 1;
-	// struct Exercise exercise;
-	//this works
-	// struct Exercise ex = { 4, 10, (12*e) };
-	// exercise = ex;
-
-	//this also works
-	// struct Exercise exercises[] = {
-	// 	{ 4, 10, (12*e++) },
-	// 	{ 4, 10, (12*e++) },
-	// 	{ 4, 10, (12*e++) },
-	// 	{ 4, 10, (12*e++) }
-	// };
-	// exercise = exercises[3];
-
-	//this don't work
-	// struct Exercise exercises[MAX_EXERCISES];
-	// exercises[e] = { 4, 10, (11*e++) };
-	// exercises[e] = { 4, 10, (11*e++) };
-	// exercises[e] = { 4, 10, (11*e++) };
-	// exercise = exercises[2];
-
-	//this don't work
-	// struct Exercise exercises[MAX_EXERCISES];
-	// exercises++ = { 4, 10, (11*e++) };
-	// exercises++ = { 4, 10, (11*e++) };
-	// exercises++ = { 4, 10, (11*e++) };
-	// exercise = exercises[2];
-
-	//this will create garbage
-	// struct Exercise exercises[MAX_EXERCISES];
-	// exercises[e].sets = 4;
-	// exercises[e].reps = 10;
-	// exercises[e].weight = 11*e;
-	// exercise = exercises[e];
-
-	//this works
-	// struct Exercise ex;
-	// ex.sets = 4;
-	// ex.reps = 10;
-	// ex.weight = 11*e;
-	// exercise = ex;
-
-	//this creates more garbage
-	// struct Exercise exercises[MAX_EXERCISES];
-	// exercises[0].sets = 4;
-	// exercises[0].sets = 10;
-	// exercises[0].weight = 35;
-	// exercise = exercises[0];
-
-	//this creates empty stuff
-	// struct Exercise exercises[MAX_EXERCISES];
-	// struct Exercise ex;
-	// ex.sets = 4;
-	// ex.sets = 10;
-	// ex.weight = 35;
-	// exercises[0] = ex;
-	// exercise = exercises[0];
