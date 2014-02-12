dee61797ae2269183c8b1bb79ff2b3286ae07e89
Igor Santos <igorsantos07@gmail.com>
2014-02-11 17:33:05 -0800

    Finishing Workout Menu interface

--- src/main.c
+++ src/main.c
@@ -4,22 +4,32 @@
 
 typedef struct {
 	char *name;
-	int sets, reps, weight;
+	int   sets,
+	      reps,
+	      weight;
 } Exercise;
 
 typedef struct {
-	char letter;
+	char      letter;
 	Exercise *exercises;
 } Workout;
+
+int icon_ids[] = { RESOURCE_ID_ICON_WORKOUT_A, RESOURCE_ID_ICON_WORKOUT_B,
+	RESOURCE_ID_ICON_WORKOUT_C, RESOURCE_ID_ICON_WORKOUT_D, RESOURCE_ID_ICON_WORKOUT_E,
+	RESOURCE_ID_ICON_WORKOUT_F, RESOURCE_ID_ICON_WORKOUT_G
+};
 
 int total_workouts = 0;
 Workout workouts[MAX_WORKOUTS];
 
+SimpleMenuLayer  *workout_menu_layer;
+SimpleMenuItem    workout_menu[MAX_WORKOUTS];
 SimpleMenuSection workout_sections[1];
-SimpleMenuItem workout_menu[MAX_WORKOUTS];
 
-Window *window;
-TextLayer *exercise_layer, *titles_layer, *numbers_layer;
+Window    *window;
+TextLayer *exercise_layer,
+          *titles_layer,
+          *numbers_layer;
 
 static char itoc(int number) { return number + 64; }
 static int ctoi(char letter) { return letter - 64; } 
@@ -29,6 +39,10 @@ static Exercise newExercise(char *name, int sets, int reps, int weight) {
 	return ex;
 }
 
+/*
+ * TODO: should stop creating workouts when total_workouts = MAX_WORKOUTS and log that instead
+ * TODO: this function should be syncronized with the workouts array!
+ */
 static Workout newWorkout(int number, Exercise *exercises) {
 	Workout w = { .letter = itoc(number), .exercises = exercises };
 	++total_workouts;
@@ -55,51 +69,58 @@ static Workout getWorkout(int key) {
 	return read_data;
 }
 
-static void loadMenu(void) {
-	struct GBitmap *icon;
-	unsigned int w;
-	int icon_ids[] = {
-		RESOURCE_ID_ICON_WORKOUT_A,
-		RESOURCE_ID_ICON_WORKOUT_B,
-		RESOURCE_ID_ICON_WORKOUT_C,
-		RESOURCE_ID_ICON_WORKOUT_D,
-		RESOURCE_ID_ICON_WORKOUT_E,
-		RESOURCE_ID_ICON_WORKOUT_F,
-		RESOURCE_ID_ICON_WORKOUT_G
-	};
-	for(w = 0; w <= sizeof(workouts); w++) {
-		char title[10];
-		Workout workout = workouts[w]; 
-		icon = gbitmap_create_with_resource(icon_ids[ctoi(workout.letter)]);
-		snprintf(title, sizeof(title), "Workout %c", workout.letter);
+static void workoutSelected(int index, void *ctx) {
+	workout_menu[index].subtitle = "<< SELECTED >>";
+	layer_mark_dirty(simple_menu_layer_get_layer(workout_menu_layer));
+}
+
+static void loadWorkoutMenu(void) {
+	for(int w = 0; w < total_workouts; w++) {
+		char *title = (char *)malloc(sizeof(char) * 10);
+		snprintf(title, 10, "Workout %c", workouts[w].letter);
 		workout_menu[w] = (SimpleMenuItem) {
 			.title    = title,
 			.subtitle = "Here be subtitles",
-			.icon     = icon
+			.callback = workoutSelected,
+			.icon     = gbitmap_create_with_resource(icon_ids[w])
 		};
 	}
 	workout_sections[0] = (SimpleMenuSection) {
-		.title     = "Workouts",
-		.num_items = w,
+		.title     = "Today's workout is...",
+		.num_items = total_workouts,
 		.items     = workout_menu
 	};
 }
 
-static void workoutSelected(int index, void *ctx) {
+static void loadExercisesMenu(void) {
+	
+}
+
+static void loadRepsScreen(void) {
+	
+}
+
+static void loadIntervalScreen(void) {
+	
+}
+
+static void loadExerciseDetailsScreen(void) {
 	
 }
 
 static void begin(void) {
 	setWorkout(1);
+	setWorkout(2);
 	workouts[0] = getWorkout(1);
+	workouts[1] = getWorkout(2);
 
 	window              = window_create();
 	Layer *window_layer = window_get_root_layer(window);
-	GRect bounds = layer_get_frame(window_layer);
-
-	loadMenu();
-	SimpleMenuLayer *menu_layer = simple_menu_layer_create(bounds, window, workout_sections, 1, NULL);
-	layer_add_child(window_layer, simple_menu_layer_get_layer(menu_layer));	
+	GRect bounds        = layer_get_frame(window_layer);
+	
+	loadWorkoutMenu();
+	workout_menu_layer = simple_menu_layer_create(bounds, window, workout_sections, 1, NULL);
+	layer_add_child(window_layer, simple_menu_layer_get_layer(workout_menu_layer));
 
 //	exercise_layer  = text_layer_create((GRect){ .origin = { 0, 0 }, .size = { .w = bounds.size.w, .h = bounds.size.h/2 } });
 //	text_layer_set_font(exercise_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
